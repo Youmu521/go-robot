a7af0226e0e34b689cc733c2a0d7e0e9a90a058b
Youmu° <1312563525@qq.com>
2023-03-03 14:09:04 +0800

    第一次推送

--- common/type.go
+++ common/type.go
@@ -1,13 +1,23 @@
 package common
 
+// 事件类型
 const (
-	EventTypeMessage = "message"
-	EventTypeRequest = "request"
-	EventTypeNotice  = "notice"
-	EventTypeMeta    = "meta_event"
+	EventTypeMessage = "message"    // 消息事件
+	EventTypeRequest = "request"    // 请求事件
+	EventTypeNotice  = "notice"     // 通知事件
+	EventTypeMeta    = "meta_event" // 元事件
 )
 
+// 消息事件类型
 const (
-	MessageTypePrivate = "private"
-	MessageTypeGroup   = "group"
+	MessageTypePrivate = "private" // 私聊消息
+	MessageTypeGroup   = "group"   // 群聊消息
+)
+
+// 通知事件类型
+
+// 请求事件类型
+const (
+	RequestTypePrivate = "private" // 好友添加请求
+	RequestTypeGroup   = "group"   // 群邀请 / 群申请
 )
